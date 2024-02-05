c6bd99fb8f91ee66630f6bb49ea852570a413a30
Илья Машков <comrademashkov@gmail.com>
2024-02-05 18:59:57 +0300

    Added warp_perspective project

---
+++ warp_perspective/main.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+
+float w = 250, h = 350;
+
+Mat matrix, imgWarp;
+
+int main(int argc, char** argv)
+{
+    const std::string img_path = "../../RESOURCES/cards.jpg";
+    Mat img = imread(img_path);
+
+    Point2f src[4] = { { 529, 142 }, { 771, 190 }, { 405, 395 }, { 674, 457 } };
+    Point2f dst[4] = { { 0.0f, 0.0f }, { w, 0.0f }, { 0.0f, h }, { w, h } };
+
+    matrix = getPerspectiveTransform(src, dst);
+    warpPerspective(img, imgWarp, matrix, Point(w, h));
+
+    imshow("Image", imgWarp);
+
+    waitKey(0);
+
+    return 0;
+}
